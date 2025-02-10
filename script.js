f234623a7ff4773db7220a060ad6d3843d9ca0bf
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-10 20:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-//complete this code
 class Animal {
-	constructor(species:string){
-		this.species=species;
-	}
-	get speci(species){
-		return `${this.species}`;
-	}
-	makeSound():void{
-		console.log(`The ${this._species} makes a sound`);
-	}
+  constructor(species) {
+    this.species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  set species(value) {
+    this._species = value;
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
 
 class Dog extends Animal {
-	constructor() {
+  constructor() {
+    super("Dog");
+  }
+
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  constructor() {
     super("Cat");
   }
 
@@ -21,22 +36,14 @@ class Dog extends Animal {
   }
 }
 
-class Cat extends Animal {
-	constructor() {
-    super("Dog");
-  }
-
-  bark() {
-    console.log("woof");
-  }
-}
+// Creating instances and testing methods
 const myCat = new Cat();
 myCat.makeSound(); // The Cat makes a sound
 myCat.purr();      // purr
 
 const myDog = new Dog();
 myDog.makeSound(); // The Dog makes a sound
-myDog.bark();
+myDog.bark();      // woof
 
 // Do not change the code below this line
 window.Animal = Animal;
